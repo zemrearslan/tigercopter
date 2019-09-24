2cf8a37401fcb5fc9c8f5396eace335bb3085f59
Diego Nuñez <dn@kialo.com>
2019-09-24 14:42:13 +0200

    implement turn upside down

--- macarena_moves.py
+++ macarena_moves.py
@@ -1,11 +1,10 @@
-from moves import Distance
-
+from moves import *
 
 async def vertical_square(dist: Distance, beats: int):
     pass
 
 
 async def turn_upside_down(beats: int):
-    pass
+    await flip(FlipDirection.RIGHT)
 
 MACARENA = [(vertical_square, 8)]
