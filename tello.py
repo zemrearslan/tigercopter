39f9c82f5137d10dddccd6160821ae328558d38c
Mathias Kahl <mk@kialo.com>
2019-09-24 15:42:52 +0200

    allow mocking Tello for test runs

--- tello.py
+++ tello.py
@@ -1,5 +1,15 @@
-from djitellopy import Tello
+from unittest.mock import Mock
 
-Tello.RESPONSE_TIMEOUT = 10
-tello = Tello()
-tello.connect()
+from djitellopy import Tello
+import sys
+
+mock = len(sys.argv) > 1 and sys.argv[1] == 'dryrun'
+
+if mock:
+    print("(MOCKING TELLO)")
+    tello = Mock(Tello)
+else:
+    tello = Tello()
+    tello.RESPONSE_TIMEOUT = 10
+    tello.TIME_BTW_COMMANDS = 1
+    tello.connect()
