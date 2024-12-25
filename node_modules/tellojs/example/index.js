71e41699e21f7e6746f775d79585a788a7e33f92
zemrearslan <zemrearslan@gmail.com>
2024-12-25 16:53:31 +0100

    Update to  body-parser@^1.20.3

--- node_modules/tellojs/example/index.js
+++
@@ -1,9 +0,0 @@
-const sdk = require('../src/index')
-
-sdk.control.connect()
-.then(() => sdk.control.takeOff())
-.then(() => sdk.control.flip.left())
-.then(() => sdk.control.flip.right())
-.then(() => sdk.control.land())
-.then(result => console.log(result))
-.catch(error => console.error(error))
--- node_modules/tellojs/src/commands/set.js
+++
@@ -1,9 +0,0 @@
-const commander = require('../exchanger')
-
-const speed = (speed) => commander.send(`speed ${speed}`)
-
-const rc = (x, y, z, yaw) => commander.send(`rc ${x} ${y} ${z} ${yaw}`)
-
-const wifi = (ssid, password) => commander.send(`wifi ${ssid} ${password}`)
-
-module.exports = { speed, rc, wifi }
